d7a5198a9ce770761d6134c19c7ccf50c0c070d4
Neel Alex <salexucb@berkeley.edu>
2017-12-15 20:33:15 -0800

    Continued work building up construction

--- Database Construction scripts/12-15-17 Construction.py
+++ Database Construction scripts/12-15-17 Construction.py
@@ -72,13 +72,54 @@ def makeSheetDatabase(worksheet, fancy=False):
     
     return db
 
+def mergeListIntoDict(targetList):
+	result = {}
+	for miniDict in targetList:
+		result = {**result, miniDict}
+
+	return result
+
 def main():
 	workbook = openpyxl.load_workbook("STP Global Resources Working Sheet - no tag listings.xlsx")
-	sheetnames = workbook.sheetnames
+	# sheetnames of workbook at first:
 	# ['Glossary', 'Non-Immersive Intern & fellow.', 'Details & Rotations', 'Fellowships', 
 	# 'Internships', 'Pairing Schemes', 'Course Syllabi', 'Degree Programs', 
 	# 'Meetings & Conferences', 'Organizations', 'Professional Networks', 'Publications', 
 	# 'Toolkits & Other Resources', 'Trainings & Workshops', 'University-Based Policy Groups']
+
+	# fancy
+	fancy = [0, 0, 0, 0, 0]
+	fancy[0] = {"remote": workbook['Non-Immersive Intern & fellow.']['A1':'BH42']}
+	fancy[1] = {"rotations" : workbook['Details & Rotations']['A1':'BH16']}
+	fancy[2] = {"fellowships" : workbook['Fellowships']['A1':'BH120']}
+	fancy[3] = {"internships" : workbook['Internships']['A1':'BH52']}
+	fancy[4] = {"pairing" : workbook['Pairing Schemes']['A1':'BH11']}
+
+	# regular
+	regular = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+	regular[0] = {"regular" : workbook['Course Syllabi']['A1':'D13']}
+	regular[1] = {"degree" : workbook['Degree Programs']['A1':'F17']}
+	regular[2] = {"meetings" : workbook['Meetings & Conferences']['A1':'G16']}
+	regular[3] = {"organizations" : workbook['Organizations']['A1':'O113']}
+	regular[4] = {"professional" : workbook['Professional Networks']['A1':'F27']}
+	regular[5] = {"publications" : workbook['Publications']['A1':'P211']}
+	regular[6] = {"toolkits" : workbook['Toolkits & Other Resources']['A1':'L9']}
+	regular[7] = {"workshops" : workbook['Trainings & Workshops']['A1':'G26']}
+	regular[8] = {"university" : workbook['University-Based Policy Groups']['A1':'F10']}
+
+	for item in fancy:
+		item[item.keys[0]] = makeSheetDatabase(item.values[0], fancy=True)
+
+	for item in regular:
+		item[item.keys[0]] = makeSheetDatabase(item.values[0], fancy=False)
+
+
+	result = mergeListIntoDict(fancy + regular)
+
+	print(result)
+
+
 
 if __name__ == '__main__':
     main()
